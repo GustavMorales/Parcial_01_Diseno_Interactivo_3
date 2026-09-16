86686a71fc8968d87571a1aec9a2f218e440911d
Gustavo Morales <Morales.fagundez@gmail.com>
2026-09-16 08:10:23 -0500

    Pacial 2 correccion de ejercicios

--- Parcial_Uno/parcial_dos.js
+++ Parcial_Uno/parcial_dos.js
@@ -27,6 +27,13 @@ function conection (status) {
   // Cuando el estado es igual a 2, el usuario está "Away"
   // De lo contrario, presumimos que el usuario está "Offline"
   // Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+    return "Online";
+  } else if (status === 2) {
+    return "Away";
+  } else {
+    return "Offline";
+  }
 }
 
 function saludo (idioma) {
@@ -36,6 +43,15 @@ function saludo (idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if (idioma === "ingles") {
+    return "Hello!";
+  } else {
+    return "Hola!";
+  }
 }
 
 function colors (color) {
@@ -46,8 +62,19 @@ function colors (color) {
   // En caso que el color recibido sea "orange", devuleve --> "This is orange"
   // Caso default: devuelve --> "Color not found"
   // Usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default:
+      return "Color not found";
+  }
 }
-
 function esDiezOCinco (numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
