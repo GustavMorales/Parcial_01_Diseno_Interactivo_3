9c8dec07fb21f9188c35e405f0d1f72bb6c3b021
Gustavo Morales <Morales.fagundez@gmail.com>
2026-09-16 14:21:19 -0500

    Pacial 2 avance de 20 ejercicios

--- Parcial_Uno/parcial_dos.js
+++ Parcial_Uno/parcial_dos.js
@@ -79,12 +79,21 @@ function esDiezOCinco (numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 function estaEnRango (numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function esEntero (numero) {
@@ -95,8 +104,8 @@ function esEntero (numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return Math.floor(numero) === numero;
 }
-
 function fizzBuzz (numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
