ec1959605cab2826a6f9e63cf12a834ad01d09b8
Gustavo Morales <Morales.fagundez@gmail.com>
2026-09-16 23:28:13 -0500

    avance 90% parcial

--- Parcial_Uno/parcial_tres.js
+++ Parcial_Uno/parcial_tres.js
@@ -154,13 +154,30 @@ function mesesDelAño (array) {
   // Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   // Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
-  // Tu código:
+  // Tu código:  
+  let nuevoArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      nuevoArray.push(array[i]);
+    }
+  }
+  if (nuevoArray.length < 3) {
+    return "No se encontraron los meses pedidos";
+  }
+  return nuevoArray;
 }
 
 function mayorACien (array) {
   // La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   // valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+ let nuevoArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 100) {
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 function breakStatement (numero) {
@@ -171,6 +188,19 @@ function breakStatement (numero) {
   // devolver: "Se interrumpió la ejecución"
   // Pista: usá el statement 'break'
   // Tu código:
+   let array = [];
+  let suma = numero;
+  for (let i = 0; i < 10; i++) {
+    suma = suma + 2;
+    if (suma === i) {
+      break;
+    }
+    array.push(suma);
+  }
+  if (array.length < 10) {
+    return "Se interrumpió la ejecución";
+  }
+  return array;
 }
 
 function continueStatement (numero) {
@@ -180,6 +210,7 @@ function continueStatement (numero) {
   // Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   // Pista: usá el statement 'continue'
   // Tu código:
+  
 }
 
 // No modificar nada debajo de esta línea
